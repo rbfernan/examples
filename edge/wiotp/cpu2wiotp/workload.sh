4aaa8b5e651bc7bb98ca5422bcce8a039a18ec88
rbfernan <rbfernan@users.noreply.github.com>
2018-02-02 13:58:59 -0200

    Sending cpu readings > 50 to the alert topic
    in addition to the status topic

--- edge/wiotp/cpu2wiotp/workload.sh
+++ edge/wiotp/cpu2wiotp/workload.sh
@@ -186,20 +186,27 @@ while true; do
       msgHost="$HZN_ORGANIZATION.messaging.$WIOTP_DOMAIN"
     fi
     
-    # Little processing sample here...
-    #If cpu > 50.0 send it to the alert topic instead
-    cpuVal=$(echo "$json" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
-    cpuLimit=50.0
-    var=$(awk 'BEGIN{print "'$cpuVal'"<"'$cpuLimit'"?1:0}')
-    if [ "$var" -eq 0 ];then
-      topic=$(echo $topic | sed -e "s/status/alert/g")
-    fi
- 
     if [[ "$VERBOSE" == 1 ]]; then
       echo mosquitto_pub -h "$msgHost" -p 8883 -i "$clientId" -u "use-token-auth" -P "$WIOTP_DEVICE_AUTH_TOKEN" --cafile $WIOTP_PEM_FILE -q 2 -t "$topic" -m "$json"
     fi
     mosquitto_pub -h "$msgHost" -p 8883 -i "$clientId" -u "use-token-auth" -P "$WIOTP_DEVICE_AUTH_TOKEN" --cafile $WIOTP_PEM_FILE -q 2 -t "$topic" -m "$json" >/dev/null
     checkrc $? "mosquitto_pub $msgHost" "continue"
+    
+    # Little extra processing sample here...
+    #If cpu > 50.0 send it to the alert topic as well
+    cpuVal=$(echo "$json" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+    cpuLimit=50.0
+    var=$(awk 'BEGIN{print "'$cpuVal'"<"'$cpuLimit'"?1:0}')
+    if [ "$var" -eq 0 ];then
+      topic=$(echo $topic | sed -e "s/status/alert/g")
+
+      if [[ "$VERBOSE" == 1 ]]; then
+        echo mosquitto_pub -h "$msgHost" -p 8883 -i "$clientId" -u "use-token-auth" -P "$WIOTP_DEVICE_AUTH_TOKEN" --cafile $WIOTP_PEM_FILE -q 2 -t "$topic" -m "$json"
+      fi
+      mosquitto_pub -h "$msgHost" -p 8883 -i "$clientId" -u "use-token-auth" -P "$WIOTP_DEVICE_AUTH_TOKEN" --cafile $WIOTP_PEM_FILE -q 2 -t "$topic" -m "$json" >/dev/null
+      checkrc $? "mosquitto_pub $msgHost" "continue"
+    fi 
+    
   fi
 
   # Pause before looping again
