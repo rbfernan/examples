cf32377274c0850110819f4d3b1548dd4f98ac0a
rbfernan <rbfernan@rbfernans-MacBook-Pro-2.local>
2018-03-16 10:21:02 -0300

    Setting correct input json file

--- edge/recipe/04-run_agent_setup.sh
+++ edge/recipe/04-run_agent_setup.sh
@@ -3,15 +3,10 @@
 echo "Running agent setup ..." 
 . ~/hzn_custom_wl_demo.env
 echo ""
-# Setting params for the hznEdgeCoreIoTInput.json.template substitution
-export WIOTP_CLASS_ID="Gateway"
-export WIOTP_DEVICE_TYPE=$WIOTP_GW_TYPE
-export WIOTP_DEVICE_ID=$WIOTP_GW_ID
-export WIOTP_DEVICE_AUTH_TOKEN=$WIOTP_GW_TOKEN
 
 echo "Updating the /etc/wiotp-edge/hznEdgeCoreIoTInput.json.template" 
 cp /etc/wiotp-edge/hznEdgeCoreIoTInput.json.template /etc/wiotp-edge/hznEdgeCoreIoTInput.json.template.orig
-envsubst <~/examples/edge/wiotp/cpu2wiotp/horizon/pattern/device/hznEdgeCoreIoTInput.json.template> /etc/wiotp-edge/hznEdgeCoreIoTInput.json.template
+envsubst <~/examples/edge/wiotp/cpu2wiotp/horizon/pattern/hznEdgeCoreIoTInput.json.template> /etc/wiotp-edge/hznEdgeCoreIoTInput.json.template
 
 echo "Configuring WIoTP Edge Node for $WIOTP_ORG_ID $WIOTP_GW_TYPE $WIOTP_GW_ID..." 
 wiotp_agent_setup --org $WIOTP_ORG_ID --deviceType $WIOTP_GW_TYPE --deviceId $WIOTP_GW_ID --deviceToken "$WIOTP_GW_TOKEN" -cn 'edge-connector'
